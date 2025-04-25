e50e11f9fdb544c2f816d0c28fe497bd8892623b
sudheer <sudheergadde01@gmail.com>
2025-04-25 15:34:12 -0500

    Setup Complete for project

---
+++ src/main/java/com/Practice/finance/InternetBankingFundTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Practice.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetBankingFundTransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetBankingFundTransferServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/finance/InternetBankingFundTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingFundTransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
